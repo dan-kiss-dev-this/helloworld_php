b57494d4e48f1802dfea116cdefba886cc72a2d7
Dan Kiss <flowfast47@gmail.com>
2018-08-14 14:52:04 -0700

    add business card marker with styling and use concat operator

---
+++ operator_practice.php
@@ -0,0 +1,21 @@
+<?php
+    $sum = 4 + 5 ;
+    $product = 4 * 2 ;
+    $difference = 12 - 5 ;
+    $quotient = 12 / 2 ;
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Math Examples</title>
+  </head>
+  <body>
+    <h1>Here is the results of some math with PHP</h1>
+    <p>Sum: <?php echo $sum; ?></p>
+    <p>Product: <?php echo $product; ?></p>
+    <p>Difference: <?php echo $difference; ?></p>
+    <p>Quotient: <?php echo $quotient; ?></p>
+  </body>
+</html>
