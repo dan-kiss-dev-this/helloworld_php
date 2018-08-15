858af73578a283b2cbf71b47e20b7248ce44e911
Dan Kiss <flowfast47@gmail.com>
2018-08-15 15:19:26 -0700

    setup function used by encrypt_form.html

--- repeater.php
+++ repeater.php
@@ -1,6 +1,8 @@
 <?php
   $repeat_text = $_GET["repeat_text"];
   $number_of_repeats = $_GET["number_of_repeats"];
+  $repeated_text = str_repeat($repeat_text, $number_of_repeats);
+  $input_phrase = $repeat_text;
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +13,6 @@
   </head>
   <body>
     <h1>Look at your phase being repeated</h1>
-    <p><?php echo str_repeat($repeat_text, $number_of_repeats);?></p>
+    <p><?php echo strtoupper($repeated_text); echo str_word_count($repeated_text) . ' words'; ?></p>
   </body>
 </html>
