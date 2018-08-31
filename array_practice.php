3a75e5a56b84d340e552c6a6b80cbb445c76e65a
Dan Kiss <flowfast47@gmail.com>
2018-08-31 11:04:49 -0700

    use a foreach loop to display elements in array

--- array_practice.php
+++ array_practice.php
@@ -1,8 +1,8 @@
 <?php
     $groceries = array("eggs", "milk", "bread");
-    var_dump($groceries);
-    array_push($groceries, 'apples', 'tomatoes');
-    var_dump($groceries);
+    // var_dump($groceries);
+    // array_push($groceries, 'apples', 'tomatoes');
+    // var_dump($groceries);
 ?>
 
 <!DOCTYPE html>
@@ -13,8 +13,13 @@
   </head>
   <body>
     <h1>My Grocery List</h1>
-    <h3><?php echo $groceries[0]; ?></h3>
-    <h3><?php echo $groceries[1]; ?></h3>
-    <h3><?php echo $groceries[2]; ?></h3>
+      <ul>
+        <?php
+          foreach($groceries as $purchase) {
+            echo '<li>' . $purchase . "</li>";
+          }
+        ?>
+        <p><?php echo $purchase ?></p>
+      </ul>
   </body>
 </html>
