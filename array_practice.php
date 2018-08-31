76aa6d3987447bcf0d5a0a2cb7119ad3f16d9baa
Dan Kiss <flowfast47@gmail.com>
2018-08-31 12:36:43 -0700

    setup associative array

--- array_practice.php
+++ array_practice.php
@@ -1,9 +1,15 @@
 <?php
-    $groceries = array("eggs", "milk", "bread", "apples", "bananas", "fruit rollups", "steak", "kale", "pasta", "flour", "sugar", "vanilla");
-    // var_dump($groceries);
-    // array_push($groceries, 'apples', 'tomatoes');
-    // var_dump($groceries);
-    $rainbow = array('red', 'orange', 'yellow', 'green', 'violet', 'blue');
+$groceries = array("eggs", "milk", "bread", "apples", "bananas", "fruit rollups", "steak", "kale", "pasta", "flour", "sugar", "vanilla");
+// var_dump($groceries);
+// array_push($groceries, 'apples', 'tomatoes');
+// var_dump($groceries);
+$rainbow = array('red', 'orange', 'yellow', 'green', 'violet', 'blue');
+$book_prices = array(25.01, 10.99, 13.50, 33, 50);
+$total_price = 0;
+$dictionary = array('apple' => 'afruit commonly red or green', 'zebra' => 'an animal with black and white stripes', "computer" => "that big shiny box you use");
+echo $dictionary["zebra"];
+$cupcake_order = array('vanilla' => 12, 'chocolate' => 24, 'raspberry' => 6, 'caramel apple' => 36 );
+$dictionary['cat'] = 'a small furry animal goes meow';
 ?>
 
 <!DOCTYPE html>
@@ -30,5 +36,12 @@
           }
         ?>
       </ul>
+      <p>Price of books</p>
+      <?php
+        foreach($book_prices as $single_price) {
+          $total_price += $single_price ;
+        }
+        echo $total_price;
+      ?>
   </body>
 </html>
