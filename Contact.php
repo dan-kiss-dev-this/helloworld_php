5f769b489dd4f2fa57a0740d89c85f994918123f
Dan Kiss <flowfast47@gmail.com>
2018-09-01 16:25:31 -0700

    add way to display images to contact.php

--- Contact.php
+++ Contact.php
@@ -4,21 +4,25 @@
         public $name;
         public $phone;
         public $address;
+        public $image_path;
     }
     $hendrix = new Contact();
     $hendrix->name = "Jimi Hendrix";
     $hendrix->phone = "503-826-9371";
     $hendrix->address = "208 se 5th st portland or 97204";
+    $hendrix->image_path = "images/hello_grand.jpg";
 
     $elvis = new Contact();
     $elvis->name = "Elvis Presley";
     $elvis->phone = "617-356-3571";
     $elvis->address = "Graceland";
+    $elvis->image_path = "images/hello_grand.jpg";
 
     $einstein = new Contact();
     $einstein->name = 'Albert Einstein';
     $einstein->phone = "415-738-4935";
     $einstein->address = "3718 MLK oakland CA";
+    $einstein->image_path = "images/hello_grand.jpg";
 
     $address_book_objects = array($hendrix, $elvis, $einstein);
 ?>
@@ -38,6 +42,7 @@
             echo "<li>";
             echo $contact->name;
             echo "<ul>";
+            echo "<li><img src='$contact->image_path'></li>";
             echo "<li> $contact->phone </li>";
             echo "<li> $contact->address </li>";
             echo "</ul>";
