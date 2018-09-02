64f713458afbf63f11ae91eb319e885eb708ee9f
Dan Kiss <flowfast47@gmail.com>
2018-09-01 17:13:16 -0700

    add sorting to cars

---
+++ car_form.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ */
+class Car
+{
+    public $make_model;
+    public $price;
+    public $miles;
+}
+
+$porsche = new Car();
+$porsche->make_model = '2014 Porshe 911';
+$porsche->price = 114000;
+$porsche->miles = 7888;
+
+$ford = new Car();
+$ford->make_model = "2011 Ford F450";
+$ford->price = 55995;
+$ford->miles = 14241;
+
+$lexus = new Car();
+$lexus->make_model = "2013 Lexus RX 350";
+$lexus->price = 44700;
+$lexus->miles = 20000;
+
+$mercedes = new Car();
+$mercedes->make_model = "Mercedes Benz CLS550";
+$mercedes->price = 39900;
+$mercedes->miles = 37979;
+
+$cars = array($porsche, $ford, $lexus, $mercedes);
+$max_price = $_GET['price'];
+$cars_matching_search = array();
+foreach ($cars as $car) {
+  if ($car->price <= $max_price) {
+    array_push($cars_matching_search, $car);
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Car dealer form</title>
+  </head>
+  <body>
+    <h1>Your dealership</h1>
+    <ul>
+      <?php
+        foreach ($cars_matching_search as $car) {
+          echo "<li> $car->make_model </li>";
+          echo "<ul>";
+            echo "<li> $car->price </li>";
+            echo "<li> Miles: $car->miles </li>";
+          echo "</ul>";
+        }
+      ?>
+    </ul>
+  </body>
+</html>
