25486d8a1ab85f0a5314c5d7a5cfc359c80cca96
Dan Kiss <flowfast47@gmail.com>
2018-09-03 18:41:36 -0700

    use private and get and set for changing cd price in cd.php

--- cd.php
+++ cd.php
@@ -4,7 +4,7 @@
     public $title;
     public $artist;
     public $cover_art;
-    public $price;
+    private $price;
 
     function __construct($album_name, $band_name, $image_path, $album_price = 10.99){
         $this->title = $album_name;
@@ -13,18 +13,32 @@
         $this->price = $album_price;
     }
 
+    function setPrice($new_price)
+    {
+        $float_price = (float) $new_price;
+        if ($float_price != 0) {
+          $formatted_price = number_format($float_price, 2);
+          $this->price = $formatted_price;
+        }
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
   }
 
   $first_cd = new CD('Master of Reality 2', 'Black Sabbath', 'images/hello_grand.jpg');
   $second_cd = new CD("Electric Ladyland","Jimi Hendrix","images/hello_grand.jpg");
-  $third_cd = new CD("Nevermind","Nirvana","images/hello_grand.jpg",15.99);
+  $fourth_cd = new CD("Nevermind","Nirvana","images/hello_grand.jpg",15.99);
   // old way here before using constructor $third_cd->title = "Nevermind";
   // $third_cd->artist = "Nirvana";
   // $third_cd->cover_art = "images/hello_grand.jpg";
   // $third_cd->price = 10.99;
 
-  $cds = array($first_cd, $second_cd, $third_cd);
-
+  $fourth_cd->setPrice("1.3925");
+  $cds = array($first_cd, $second_cd, $fourth_cd);
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +52,7 @@ strap/3.2.0/css/bootstrap.min.css'>
     <div class="container">
         <?php
             foreach ($cds as $album) {
+              $cd_price = $album->getPrice();
                 echo "<div class='row'>
                     <div class='col-md-6'>
                       <img src='$album->cover_art'>
@@ -45,7 +60,7 @@ strap/3.2.0/css/bootstrap.min.css'>
                     <div class='col-md-6'>
                         <p>$album->title</p>
                         <p>By $album->artist</p>
-                        <p>$$album->price</p>
+                        <p>$$cd_price</p>
                     </div>
                 </div>
             "; }
